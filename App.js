7d8684cb28c76a943b7a37e9f84b16753db87b1f
BhoomaSivajyothi <jyothikesava@gmail.com>
2023-08-04 14:11:13 +0530

    filess added

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//const Tittle= function ()
+//{
+   // return (
+    //<h1>this normal javascript function we use instilized like this react function
+  //</h1>)
+
+  // }
+
+let AnotherComponent = ()=>(
+    <div><h1 id="jyo"> this anthor component and arrow function</h1></div>
+)
+let title = <h1>this is jsx element</h1>
+const number=2000;
+const  Headingcomponent = () =>{
+    return(<div>
+        {number},
+        {1000},
+        {title},
+        <AnotherComponent/>,
+      
+       <data />
+        <h1 className="heading">Nameste React functional component</h1>
+    </div>)}
+ const roots = ReactDOM.createRoot(document.getElementById("root"))
+ roots.render(<Headingcomponent/>)
