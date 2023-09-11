1b0f415caf5e352db9a19e08109879e5c30dfe6d
BhoomaSivajyothi <jyothikesava@gmail.com>
2023-09-11 19:26:01 +0530

    veg and non veg lable &hoc added

--- src/Components/Foodcard.js
+++ src/Components/Foodcard.js
@@ -5,14 +5,19 @@ export  const Card = (props) =>{
     const {resData} = props || {}
   
     const {
-        name, cloudinaryImageId, costForTwo, cuisines, avgRating, sla,
+        name, cloudinaryImageId, costForTwo, cuisines, avgRating, sla, veg
     } = resData?.info || {}
   
     const { deliveryTime } = sla
 
      
     return (
-        
+        <>
+        {veg && veg === true? <div>
+        <label className=" border-2 border-black  absolute rounded m-2 px-1"> veg</label>
+        </div>:<div>
+        <label className=" border-2 border-black  absolute rounded m-2 px-1">non veg</label>
+        </div> }
         <div className="res-card w-52 h-[450px] border-spacing-0 rounded-sm  bg-gray-100 hover:bg-gray-300 m-4 p-4 hover:bg-gr" >
             <img className="w-44  h-44"
             src={CDN_URL + cloudinaryImageId}
@@ -24,13 +29,14 @@ export  const Card = (props) =>{
             <h4>{deliveryTime}</h4>
             <h4>{props.label}</h4>
         </div>
+        </>
     )
   }
  export const withveglable = (Card) =>{
   return (resData) =>{
     return(
        <div>
-        <label className=" border-2 border-black  absolute rounded m-2 px-1">veg</label>
+        <label className=" border-2 border-black text-red-400  absolute rounded mx-40 px-1">veg-Hoc</label>
         <Card {...resData}/>
         </div>  
     )
